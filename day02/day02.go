476e92000f71228f0bb4d8b0c51a31abcccfe4e0
angu2106 <antongunnarsson02@gmail.com>
2024-12-02 20:43:45 +0100

    Day 2 part 2 done

--- day02/day02.go
+++ day02/day02.go
@@ -7,6 +7,14 @@ import (
 	"strings"
 )
 
+func isSafeLine(arr []int, maxDiff int) bool {
+	if isIncreasingWithMaxDiff(arr, maxDiff) || isDecreasingWithMaxDiff(arr, maxDiff) {
+		return true
+	} else {
+		return false
+	}
+}
+
 func isIncreasingWithMaxDiff(arr []int, maxdiff int) bool {
 	for i := 1; i < len(arr); i++ {
 		if arr[i] <= arr[i-1] || arr[i]-arr[i-1] > maxdiff || arr[i] == arr[i-1] {
@@ -28,7 +36,7 @@ func isDecreasingWithMaxDiff(arr []int, maxdiff int) bool {
 }
 
 func RunDay02() {
-	inputFile, err := os.ReadFile("day02/example.txt")
+	inputFile, err := os.ReadFile("day02/input.txt")
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		return
@@ -37,11 +45,10 @@ func RunDay02() {
 	rows := strings.Split(string(inputFile), "\n")
 	//fmt.Println(inputFile)
 	part1(rows)
-	//part2()
+	part2(rows)
 }
 func part1(rows []string) {
 	fmt.Println("###### DAY 2 Part 1 ######")
-
 	count := 0
 	for _, row := range rows {
 		columns := strings.Fields(row)
@@ -56,14 +63,45 @@ func part1(rows []string) {
 			intArray[i] = num
 		}
 
-		if isIncreasingWithMaxDiff(intArray, 3) || isDecreasingWithMaxDiff(intArray, 3) {
+		if isSafeLine(intArray, 3) {
 			count++
 		}
 
 	}
+	fmt.Printf("PART 1: %d\n", count)
 
-	fmt.Println(count)
 }
-func part2() {
+func part2(rows []string) {
 	fmt.Println("###### DAY 2 Part 2 ######")
+	count := 0
+	for _, row := range rows {
+		columns := strings.Fields(row)
+		intArray := make([]int, len(columns))
+
+		for i, str := range columns {
+			num, err := strconv.Atoi(str)
+			if err != nil {
+				fmt.Printf("Error converting %s to integer: %v\n", str, err)
+				return
+			}
+			intArray[i] = num
+		}
+
+		if isSafeLine(intArray, 3) {
+			count++
+		} else {
+			for index := 1; index < len(intArray); index++ {
+
+				intArray = append(intArray[:index], intArray[index+1:]...)
+
+				if isSafeLine(intArray, 3) {
+					count++
+					break
+				}
+			}
+
+		}
+
+	}
+	fmt.Printf("PART 2: %d", count)
 }
